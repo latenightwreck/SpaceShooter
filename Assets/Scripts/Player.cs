245efd32749d8bb62dedb8dcff242ad84be9f70f
latenightwreck <morgan.b.armstrong@gmail.com>
2020-06-11 22:29:41 -0400

    First Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    void Start()
+    {
+        transform.position = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    void CalculateMovement() {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput);
+
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0));
+
+        //To allow for screen wrapping
+        if (transform.position.x >= 11.3f)
+        {
+        transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+        transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+}
