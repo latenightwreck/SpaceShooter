e49a526237507a3e30ac9cfc628dc69421f6ca45
Morgan Blaze <3926332+latenightwreck@users.noreply.github.com>
2020-06-11 23:19:22 -0400

    Added destroying of Lasers. Added cooldown of lasers.

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -7,15 +7,13 @@ public class Laser : MonoBehaviour
 
     [SerializeField]
     private float _speed = 8f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
+        if(transform.position.y >= 8f) {
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,8 +7,14 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private float _speed = 3.5f;
+
     [SerializeField]
     private GameObject _laserPrefab;
+
+    [SerializeField]
+    private float _fireRate = 0.15f;
+    private float _canFire = 0f;
+
     void Start()
     {
         transform.position = new Vector3(0,0,0);
@@ -18,9 +24,9 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
-
-        if(Input.GetKeyDown(KeyCode.Space)) {
-            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        
+        if(Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire ) {
+            ShootLaser();
         }
     }
 
@@ -44,5 +50,10 @@ public class Player : MonoBehaviour
         {
         transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
+    }
+
+    void ShootLaser() {
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, .8f, 0), Quaternion.identity);
     }
 }
